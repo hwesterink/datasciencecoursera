ce869eeff57fa515fab9ed4bf72d5fa50a9e3a72
Henk Westerink <h.p.westerink@kpnmail.nl>
2016-03-26 08:44:49 +0100

    Scripts quiz week 1 Getting and Cleaning Data changed.

--- 3_Script_quiz_w1-q3.R
+++ 3_Script_quiz_w1-q3.R
@@ -1,7 +1,7 @@
 ## Downloading data
-## Download date = 24-3-2016
+## Download date = 25-3-2016
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(fileUrl, destfile="R Scripts/Data/NGPA_data.xlsx")
+download.file(fileUrl, destfile="R Scripts/Data/NGPA_data.xlsx", mode="wb")
 dataDownload <- date()
 ## Install xlsx-package
 ## Loading the library asks to install Java
@@ -10,7 +10,7 @@ library(xlsx)
 ## Loading data into R
 colIndex <- 7:15
 rowIndex <- 18:23
-dat = read.xlsx("R Scripts/Data/NGPA_data.xlsx", sheetindex=1, header=TRUE, colIndex=colIndex, rowIndex=rowIndex)
+dat = read.xlsx("./R Scripts/Data/NGPA_data.xlsx", sheetIndex=1, colIndex=colIndex, rowIndex=rowIndex)
 head(dat)
 ## Data analysis
 print(sum(dat$Zip*dat$Ext,na.rm=T))
